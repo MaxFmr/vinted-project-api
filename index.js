41d50854429af9d3a2bb254adeee6756f0176d59
Maxime Kerlidou <maximekerlidou@air-de-maxime.lan>
2021-10-22 14:53:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const formidableMiddleware = require("express-formidable");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+
+const app = express();
+app.use(formidableMiddleware());
+app.use(cors( ));
+// Connexion à la BDD nommée "students-app" :
+mongoose.connect(process.env.MONGODB_URI);
+
+//config Cloudinary
+
+cloudinary.config({
+  cloud_name: "dal6ewwdl",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Import des routes
+const loginRoutes = require("./routes/user");
+app.use(loginRoutes);
+
+const signupRoutes = require("./routes/user");
+app.use(signupRoutes);
+
+const publishRoutes = require("./routes/Offers");
+app.use(publishRoutes);
+//Modeles
+
+const uid2 = require("uid2");
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
